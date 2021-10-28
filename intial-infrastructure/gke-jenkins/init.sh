fc2aba9b86594f3275017d76df5a3d492e61e684
Dmitry Krasaev <dmitry_krasaev@epam.com>
2021-10-28 16:05:10 +0300

    tes

--- intial-infrastructure/gke-jenkins/init.sh
+++ intial-infrastructure/gke-jenkins/init.sh
@@ -2,9 +2,10 @@
 
 # set current project
 PROJECT_ID=$(gcloud config get-value project)
+STATE_BUCKET_NAME=${PROJECT_ID}-tfstate
 
 # create tf state bucket
-gsutil mb gs://${PROJECT_ID}-tfstate
+gsutil mb gs://"$STATE_BUCKET_NAME"
 gsutil versioning set on gs://"${PROJECT_ID}"-tfstate
 
 wget https://releases.hashicorp.com/terraform/1.0.9/terraform_1.0.9_linux_amd64.zip
@@ -13,7 +14,7 @@ sudo mv terraform /usr/local/bin/
 rm terraform_1.0.9_linux_amd64.zip
 
 # cd jenkins-gke/tf-gke/
-terraform init
+terraform init -backend-config="bucket=$STATE_BUCKET_NAME"
 # terraform plan --var "github_username=$GITHUB_USER" --var "github_token=$GITHUB_TOKEN"
 # terraform apply --auto-approve --var "github_username=$GITHUB_USER" --var "github_token=$GITHUB_TOKEN"
 #
